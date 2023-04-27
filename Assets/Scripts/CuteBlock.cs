094b97fca85d2236542d0fbbdb6d4028711ca2d6
“chaoshen“ <790166359@qq.com>
2023-04-27 16:32:11 +0800

    第一次提交

--- Assets/Scripts/CuteBlock.cs
+++ Assets/Scripts/CuteBlock.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using DefaultNamespace;
-using DG.Tweening;
 using UnityEngine;
 
 public class CuteBlock : MonoBehaviour
 {
     public int index;
-    private float previousTime;
-    public static int height = 20;
-    public static int width = 10;
-    private static Transform[,] grid = new Transform[width, height];
     [SerializeField]
     private Transform childContent;
     public List<BlockItem> childList=new List<BlockItem>();
@@ -98,116 +91,6 @@ public class CuteBlock : MonoBehaviour
             }
         }
         return false;
-    }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        /*if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            //rotate !
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
-            if (!ValidMove())
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
-        }
-
-
-        /*if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
-        {
-            transform.position += new Vector3(0, -1, 0);
-            if (!ValidMove())
-            {
-                transform.position -= new Vector3(0, -1, 0);
-                AddToGrid();
-                CheckForLines();
-
-                this.enabled = false;
-                FindObjectOfType<SpawnTetromino>().NewTetromino();
-
-            }
-            previousTime = Time.time;
-        }*/
-    }
-
-    void CheckForLines()
-    {
-        for (int i = height-1; i >= 0; i--)
-        {
-            if(HasLine(i))
-            {
-                DeleteLine(i);
-                RowDown(i);
-            }
-        }
-    }
-
-    bool HasLine(int i)
-    {
-        for(int j = 0; j< width; j++)
-        {
-            if (grid[j, i] == null)
-                return false;
-        }
-
-        return true;
-    }
-
-    void DeleteLine(int i)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            Destroy(grid[j, i].gameObject);
-            grid[j, i] = null;
-        }
-    }
-
-    void RowDown(int i)
-    {
-        for (int y = i; y < height; y++)
-        {
-            for (int j = 0; j < width; j++)
-            {
-                if(grid[j,y] != null)
-                {
-                    grid[j, y - 1] = grid[j, y];
-                    grid[j, y] = null;
-                    grid[j, y - 1].transform.position -= new Vector3(0, 1, 0);
-                }
-            }
-        }
-    }
-
-
-
-
-    void AddToGrid()
-    {
-        foreach (Transform children in transform)
-        {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-
-            grid[roundedX, roundedY] = children;
-        }
-    }
-
-    bool ValidMove()
-    {
-        foreach (Transform children in transform)
-        {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-
-            if(roundedX < 0 || roundedX >= width || roundedY < 0 ||roundedY >= height)
-            {
-                return false;
-            }
-
-            if (grid[roundedX, roundedY] != null)
-                return false;
-        }
-
-        return true;
     }
 
     public void ResetPos(Transform box)
